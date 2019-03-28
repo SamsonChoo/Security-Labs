44b23d2853df161c2b5694c23e1682fded53b893
SamsonChoo <hanye_choo@mymail.sutd.edu.sg>
2019-03-28 14:10:30 +0800

    Lab 6 done

--- Lab 6/ecb.py
+++ Lab 6/ecb.py
@@ -1,15 +1,46 @@
 #!/usr/bin/env python3
 # ECB wrapper skeleton file for 50.020 Security
 # Oka, SUTD, 2014
+# Samson, SUTD, 2019
+
 from present import *
 import argparse
+import binascii
 
 nokeybits=80
 blocksize=64
 
 
 def ecb(infile,outfile,key,mode):
-    pass
+    in_file = open(infile, mode = 'rb')
+    key_file = open(key,mode='r')
+    out_file = open(outfile, mode = 'wb+')
+    initkey = int(key_file.read(),16)
+    
+    if (mode.lower()=="e"):
+        operation = present
+    if (mode.lower()=="d"):
+        operation = present_inv
+    
+    output=[]
+    
+    block = in_file.read(8)
+    while (block != b''):
+        blockout = ''
+        for i in block:
+            blockout += format(i, '08b')
+        blockout = int(blockout, 2)
+
+        output.append(binascii.unhexlify('%016x' % operation(blockout, initkey)))
+
+        block = in_file.read(8)
+
+    for byte in output:
+        out_file.write(byte)
+        
+    in_file.close()
+    key_file.close()
+    out_file.close()
 
 if __name__=="__main__":
     parser=argparse.ArgumentParser(description='Block cipher using ECB mode.')
@@ -22,6 +53,8 @@ if __name__=="__main__":
     infile=args.infile
     outfile=args.outfile
     keyfile=args.keyfile
+    mode=args.mode
 
+    ecb(infile,outfile,keyfile,mode)
 
 
